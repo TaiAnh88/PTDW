48ed5725d69d2ab25133cbf49ad0607360a6f47f
TaiAnh88 <tranthienbao050@gmail.com>
2024-12-01 15:27:18 +0700

    th12

---
+++ PTDW/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTDW.Models;
+
+namespace PTDW.Controllers
+{
+    public class ContactController: Controller
+    {
+        private readonly HarmicContext _context;
+        public ContactController(HarmicContext Context)
+        {
+            _context = Context;
+        }
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult Create(string name,  string phone, string email, string message, string CreatedDate)
+        {
+            try {
+                TblContact contact = new TblContact();
+                contact.Name = name;
+                contact.Phone = phone;
+                contact.Email = email;
+                contact.Message = message;
+                contact.CreatedDate = DateTime.Now;
+                _context.Add(contact);
+                _context.SaveChangesAsync();
+                return Json(new { startus = true });
+                    }
+            catch
+            {
+                return Json(new { startus = false });
+            }
+        }
+    }
+}
